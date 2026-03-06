94d7f9ebc3e0dfb1c5d16dcc3f1b26939df1b503
m27oliveira89-prog <m27oliveira89@gmail.com>
2026-03-06 19:19:15 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 st.markdown("<h1 style='text-align: center; color: #D4AF37;'>PAINEL DO GESTOR</h1>", unsafe_allow_html=True)
 st.markdown("---")
 st.subheader("Histórico de Batidas (Tempo Real)")
-     try:
+try:
         dados = db.reference('batidas').get()
         if dados:
             registros = []
