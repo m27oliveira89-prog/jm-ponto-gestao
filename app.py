936419158aa423d729ed381ba5e9317b19911437
m27oliveira89-prog <m27oliveira89@gmail.com>
2026-03-06 19:17:27 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 st.markdown("<h1 style='text-align: center; color: #D4AF37;'>PAINEL DO GESTOR</h1>", unsafe_allow_html=True)
-    st.markdown("---")
+st.markdown("---")
     
     st.subheader("Histórico de Batidas (Tempo Real)")
     
