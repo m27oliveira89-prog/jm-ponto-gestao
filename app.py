baaeb939872e26352e1b7aeea87fb9dc51f04672
m27oliveira89-prog <m27oliveira89@gmail.com>
2026-03-06 18:51:31 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-import streamlit as st
-import pandas as pd
-try:
+# --- 5. ÁREA DO RH (ATUALIZADA COM EXPORTAÇÃO) ---
+else:
+    st.markdown("<h1 style='color: #D4AF37; text-align: center;'>PAINEL DO GESTOR</h1>", unsafe_allow_html=True)
+    st.markdown("---")
+    
+    st.subheader("Histórico de Batidas (Tempo Real)")
+    
+    try:
         dados = db.reference('batidas').get()
         if dados:
             registros = []
@@ -40,8 +45,3 @@ try:
             st.info("Aguardando os primeiros registros...")
     except Exception as e:
         st.error(f"Erro ao carregar dados: {e}")
-streamlit
-firebase-admin
-pandas
-Pillow
-geopy
